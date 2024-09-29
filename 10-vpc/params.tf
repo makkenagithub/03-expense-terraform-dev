ab4e78784841ae2e7eb5c3842d0abb445ed0ef5a
makkenagithub <suresh.makkena@gmail.com>
2024-09-29 19:32:59 +0530

    expense project using terraform

--- 10-vpc/params.tf
+++ 10-vpc/params.tf
@@ -12,3 +12,17 @@ resource "aws_ssm_parameter" "public_subnet_ids" {
   # join function is to conver list to list of strings - string1, string2 etc
   value = join(",", module.vpc.public_subnet_ids)
 }
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project_name}/${var.env}/private_subnet_ids"
+  type  = "StringList"
+  # join function is to conver list to list of strings - string1, string2 etc
+  value = join(",", module.vpc.private_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project_name}/${var.env}/database_subnet_ids"
+  type  = "StringList"
+  # join function is to conver list to list of strings - string1, string2 etc
+  value = join(",", module.vpc.database_subnet_ids)
+}
